e84b323d94761b8a036f350822670a7cd29b5699
8ar7ar <5ar7ar@mail.ru>
2023-10-30 19:59:25 +0500

    stage0-module6-task1 " 1st attempt. Locally tested. Mon 30 oct 2023"

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFour) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFour;
+    }
+
+    public String getDescription() {
+        String four = this.hasFur ? "a" : "no";
+        String pawS = this.numberOfPaws == 1 ? "paw" : "paws";
+        String description = "This animal is mostly " + color +
+                             ". It has " + numberOfPaws + " " + pawS +
+                             " and " + four + " fur.";
+        return description;
+    }
 
 }
